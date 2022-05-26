c25b76206acf70d7f434b0e43aec464d747d5230
anuvrinda joshi <dr.joshi1999@gmail.com>
2022-05-26 18:49:34 +0530

    day 2,three questions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -20,3 +20,40 @@ return ar;
 }
 console.log(fizzbuzz(15));
 
+//day 2. question 1, https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+var numberOfSteps = function(num) {
+    let i=0;
+while(num>0){
+    if(num%2==0){
+        num= (num/2);
+    }else{
+        num=num-1;
+    }
+    i++;
+}
+    return i;
+};
+console.log(numberOfSteps(8));
+
+//day 2., question 2,https://leetcode.com/problems/defanging-an-ip-address/
+var defangIPaddr = function(address) {
+
+    let arr= address.split('');
+        
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].includes('.')){
+            arr[i]='[.]'; 
+        }
+    }
+     return arr.join('');
+    }
+console.log(defangIPaddr('255.100.50.0'))
+
+// day 2, question 3, https://leetcode.com/problems/concatenation-of-array/
+var getConcatenation = function(nums) {
+    let ans= nums.concat(nums);
+    return ans;
+};
+console.log(getConcatenation([1,2,1]));
+
+
