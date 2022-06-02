63f9b1662d8c024b6d94eb171a72bd992ffccf62
anuvrinda joshi <dr.joshi1999@gmail.com>
2022-06-02 16:48:46 +0530

    added new file

---
+++ runningSum.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/running-sum-of-1d-array/
+var runningSum = function(nums) {
+    for(let i=1;i<nums.length;i++){
+        nums[i]=nums[i]+nums[i-1]
+        }
+    return nums;
+};
+console.log(runningSum([1,2,3,4,5]));
