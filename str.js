7f8257454db934d9416f095e287741d7c4e30885
anuvrinda joshi <dr.joshi1999@gmail.com>
2022-06-12 23:33:11 +0530

    ques

--- str.js
+++ str.js
@@ -1,24 +1,8 @@
-//https://leetcode.com/problems/implement-strstr/
+// question https://leetcode.com/problems/implement-strstr/
 
-var strStr = function(haystack, needle) {
+let strStr = function(haystack, needle) {
     return haystack.indexOf(needle);
 };
 console.log(strStr('hello','ll'));
 
-/*
-var twoSum = function(nums, target) {
-  
-    for(let i=0;i<nums.length;i++){
-        
-        let sum= [(nums[i]),(nums[i+1])]
-        if(nums[i]+nums[i+1]==target){
-            let a=nums.indexOf(nums[i]);
-            let b =nums.indexOf(nums[i+1])
-            let c=[a,b]
-            return c;
-        }
-    }
- };
 
- console.log(twoSum([2,7,11,15],9));
- */
