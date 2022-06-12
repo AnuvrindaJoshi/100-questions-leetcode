1fc7f5d6f22918cee9bbbfcaf98134b29414a905
anuvrinda joshi <dr.joshi1999@gmail.com>
2022-06-12 23:24:57 +0530

    deleted wrong question

--- str.js
+++ str.js
@@ -4,3 +4,21 @@ var strStr = function(haystack, needle) {
     return haystack.indexOf(needle);
 };
 console.log(strStr('hello','ll'));
+
+/*
+var twoSum = function(nums, target) {
+  
+    for(let i=0;i<nums.length;i++){
+        
+        let sum= [(nums[i]),(nums[i+1])]
+        if(nums[i]+nums[i+1]==target){
+            let a=nums.indexOf(nums[i]);
+            let b =nums.indexOf(nums[i+1])
+            let c=[a,b]
+            return c;
+        }
+    }
+ };
+
+ console.log(twoSum([2,7,11,15],9));
+ */
