369737e9b88c716c2c27838a791ee21247029061
anuvrinda joshi <dr.joshi1999@gmail.com>
2022-06-15 14:06:16 +0530

    new ques

---
+++ longestSubstring.js
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+var lengthOfLongestSubstring = function(s) {
+    let left=0;
+    let right=0;
+    let maxLength=0;
+    let set = new Set();
+
+while(right<s.length){
+    if(!set.has(s.charAt(right))){
+        set.add(s.charAt(right));
+        maxLength= Math.max(maxLength,set.size);
+        right++;
+    }else{
+        set.delete(s.charAt(left));
+        left++;
+    }
+}
+    return maxLength;
+};
+console.log(lengthOfLongestSubstring('abcabcbb'));
